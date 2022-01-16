ccd0699d22730fba2137e85fa1b94964233d91a1
murphy <1243917465@qq.com>
2022-01-16 11:58:24 +0800

    实现 jQuery demo

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const $div = $('<div><span>1</span></div>')
+const $childList = $('.child')
+$('body').append($childList)
+
+// const $api = $('.test').find('.child').addClass('black')
+// .end().addClass('red')
+
+// const $api = $('.test').find('.child').parent().print()
